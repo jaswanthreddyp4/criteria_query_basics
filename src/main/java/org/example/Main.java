64d3f281472fcc59b797f83c4c23fb8628069d12
Jaswanth reddy <1ms21is078@gmail.com>
2024-12-18 07:37:11 +0530

    added one more example of using predicates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,7 +13,7 @@ import org.hibernate.jpa.HibernatePersistenceProvider;
 
 import java.util.*;
 
-public class Main {
+public class  Main {
     public static void main(String[] args) {
         Map<String,String> propertyMap = new HashMap<>();
 
@@ -118,6 +118,34 @@ public class Main {
             query.getResultList().forEach(System.out::println);
 
              */
+
+            /*
+
+            int x=1000;
+            int y=2000;
+            String gender="m";
+            CriteriaQuery<Product> criteriaQuery= builder.createQuery(Product.class);
+            Root<Product> productRoot = criteriaQuery.from(Product.class);
+            List<Predicate> predicateList = new ArrayList<>();
+
+            if(x!=0 && y!=0){
+                predicateList.add(builder.and(
+                        builder.le(productRoot.get("price"),y),
+                        builder.ge(productRoot.get("price"),x)
+                ));
+            }
+
+            if(gender!=null && !gender.isEmpty()){
+                predicateList.add(builder.equal(productRoot.get("gender"),gender));
+            }
+            criteriaQuery.select(productRoot);
+
+            TypedQuery<Product> query = em.createQuery(criteriaQuery);
+            query.getResultList().forEach(System.out::println);
+
+             */
+
+
 
             em.getTransaction().commit();
         }finally{
